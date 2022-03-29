5ac49d859daac5aaa961068fc82c983c9efee3b7
tadamcz <tmkadamcz@gmail.com>
2022-03-29 19:58:42 +0100

    add metalogistic prior

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy import stats
+from metalogistic import MetaLogistic
 
 from simulation import Simulation
 
-prior_mu, prior_sigma = 1, 1
-prior = stats.lognorm(scale=np.exp(prior_mu), s=prior_sigma)
+prior_ps = [.15, .5, .9]
+prior_xs = [-20, 5, 50]
+prior = MetaLogistic(cdf_ps=prior_ps, cdf_xs=prior_xs)
 study_sample_size = 100
 population_std_dev = 20
 bar = 5
@@ -13,5 +15,5 @@ simulation = Simulation(
 	study_sample_size=study_sample_size,
 	population_std_dev=population_std_dev,
 	bar=bar)
-simulation.run(max_iterations=100)
+simulation.run(max_iterations=1000)
 
