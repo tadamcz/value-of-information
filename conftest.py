7a751ac06e38a0de6720a27c74e12f3d18550dab
tadamcz <tmkadamcz@gmail.com>
2022-04-17 20:51:33 +0100

    cleanup unused

--- conftest.py
+++ conftest.py
@@ -4,17 +4,6 @@ import pytest
 import tests.shared
 
 
-def pytest_addoption(parser):
-	parser.addoption(
-		"--cmdopt", action="store", default="type1", help="my option: type1 or type2"
-	)
-
-
-@pytest.fixture
-def cmdopt(request):
-	return request.config.getoption("--cmdopt")
-
-
 def seed_idfn(fixture_value):
 	return f"seed={fixture_value}"
 
