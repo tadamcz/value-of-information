ec0d260935827c3c6fb3ab0867942dccd7d67fed
tadamcz <tmkadamcz@gmail.com>
2022-04-17 08:27:17 +0100

    use multiple CPUs in pytest_extra_slow.sh

--- pytest_extra_slow.sh
+++ pytest_extra_slow.sh
@@ -3,4 +3,4 @@ LOG_FILE=pytest_extra_slow.log
 USE_MULTIPLE_SEEDS=1
 exec > >(tee -a ${LOG_FILE}) 2>&1
 echo "running $(basename $BASH_SOURCE) at $(date)"
-pytest -m "extra_slow"
+pytest -m "extra_slow" -n auto
