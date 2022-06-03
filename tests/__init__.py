97bd4a34711c57e2d62b6a62e9795862b891fdab
tadamcz <tmkadamcz@gmail.com>
2022-06-03 18:11:20 +0100

    add a test for our claims about the standard error of mean VOI

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,2 @@
 """
-todo test ideas:
-1. run many (100?) simulations and check that mean_value_study has the expected error distribution
-(approximately normal with the correct standard error).
 """
