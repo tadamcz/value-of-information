5cb534d6f4625c79c02b4733bfb508c7dce3bb3b
tadamcz <tmkadamcz@gmail.com>
2022-06-04 14:10:47 +0100

    tweaks to explicit_bayes.py

--- tests/unit/explicit_bayes.py
+++ tests/unit/explicit_bayes.py
@@ -1,14 +1,15 @@
+from unittest.mock import patch
+
 import numpy as np
 import pytest
 
+import tests.param_generators.lognorm_norm as gen_lgn_n
 import tests.param_generators.norm_norm as gen_n_n
 import tests.shared as shared
-from value_of_information.voi import solve_threshold_b
-from value_of_information.voi import value_of_information
+from value_of_information.voi import solve_threshold_b, value_of_information
 
 
-@pytest.mark.parametrize('params', gen_n_n.linsp_distance_to_bar(2), ids=shared.sim_param_idfn)
-def test(params):
+def helper(params):
 	sd_B = params.sd_B
 	bar = params.bar
 	prior_T = params.prior_T
@@ -23,3 +24,15 @@ def test(params):
 
 			for key in ['w_out_signal', 'payoff_w_out_signal', 'w_signal', 'payoff_w_signal', 'VOI']:
 				assert threshold[key] == explicit[key]
+
+
+@pytest.mark.parametrize('params', gen_lgn_n.linsp(4), ids=shared.sim_param_idfn)
+def test_arbitrary(params):
+	helper(params)
+
+
+@pytest.mark.parametrize('params', gen_n_n.linsp(6) + gen_n_n.from_seed(10), ids=shared.sim_param_idfn)
+def test_closed_form(params):
+	with patch('value_of_information.bayes.posterior') as patched_posterior:
+		patched_posterior.side_effect = shared.normal_normal_closed_form
+		helper(params)
