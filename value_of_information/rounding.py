96e3ec71dad19321e0fec05d645348bb5fd72e91
tadamcz <tmkadamcz@gmail.com>
2022-06-17 16:57:59 +0100

    improve rounding by forcing output to be a string

--- value_of_information/rounding.py
+++ value_of_information/rounding.py
@@ -4,14 +4,14 @@ from sigfig import round as round_lib
 from value_of_information import constants
 
 
-def round_sig(x, sig=constants.ROUND_SIG_FIG, *args, **kwargs):
-	if isinstance(x, np.float64):
+def round_sig(x, sig=constants.ROUND_SIG_FIG, out_type=str, *args, **kwargs):
+	if isinstance(x, (np.float64, np.int64)):
 		if np.isnan(x):
 			return "NaN"
-		return round_lib(float(x), sig, *args, **kwargs)
+		return round_lib(float(x), sig, type=out_type, *args, **kwargs)
 	if isinstance(x, (float, int)):
-		return round_lib(x, sig, *args, **kwargs)
+		return round_lib(x, sig, type=out_type, *args, **kwargs)
 	if isinstance(x, (list, np.ndarray)):
-		return [round_sig(i, sig, *args, **kwargs) for i in x]
+		return [round_sig(i, sig, out_type=out_type, *args, **kwargs) for i in x]
 	else:
-		raise TypeError("Must be numeric or list/array of numerics")
+		raise TypeError(f"Must be numeric or list/array of numerics, not {type(x)}")
